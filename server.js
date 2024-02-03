fb5cdc5a178f7f68abc1aafaf47ff13083516152
Kariuki Antony <antonygichoya9@gmail.com>
2024-02-03 17:58:16 +0300

    setting up the server

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const logger = require("morgan");
+const dotenv = require("dotenv");
+const createError = require("http-errors");
+
+dotenv.config();
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger("dev"));
+
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    "Hello there": "Welcome to the amredi app",
+  });
+});
+
+// error handling
+app.use(async (req, res, next) => {
+  const error = createError.NotFound("The page does not exist");
+  next(error);
+});
+
+app.use(async (error, req, res, next) => {
+  const status = error.status || 500;
+  res.status(status).json({
+    message: error.message,
+    status: error.status,
+  });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, (error) => {
+  if (error) {
+    console.error(`An error has occured: ${error}`);
+  }
+  console.log(`app running on http://127.0.0.1:${port}`);
+});
