a2386c1c8ed22e0843c42c2d01c3f78fc80b7f85
David-mwas <dmwas704@gmail.com>
2024-02-18 17:41:50 +0300

    Add :create Project route

---
+++ routes/project.js
@@ -0,0 +1,14 @@
+const projectRouter = require("express").Router();
+
+const projectController = require("../controllers/projectController");
+const { verifyAccessToken } = require("../helpers/getJwt");
+
+projectRouter.post(
+  "/project/group/:groupId/create",
+  verifyAccessToken,
+  projectController.createProject
+);
+
+
+
+module.exports = { projectRouter };
