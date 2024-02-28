391d1266757d847cafcfa1485cbe00f4042f064f
David-mwas <dmwas704@gmail.com>
2024-02-28 20:58:47 +0300

    Update : fix create post controlers to handle optional image req

--- controllers/postController.js
+++ controllers/postController.js
@@ -12,45 +12,68 @@ exports.createPost = async (req, res) => {
   console.log(req.payload.aud);
   // const createdBy = req.payload.aud;
   try {
-    const imageFile = req.files[0]; // Assuming only one file is uploaded
-    if (!imageFile) {
-      return res.status(400).json({ error: "No file uploaded" });
+    // Assuming only one file is uploaded
+    if (req.files) {
+      const imageFile = req?.files[0];
+      const user = await userModel.findById(req.payload.aud);
+      // console.log(user);
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      // Access other form fields from req.body
+      const { title, description } = req.body;
+
+      // Convert image buffer to Base64 string
+      const imageBuffer = imageFile?.buffer?.toString("base64");
+
+      // Upload the image to Cloudinary as Base64 string
+      const result = await cloudinary.uploader.upload(
+        `data:${imageFile.mimetype};base64,${imageBuffer}`,
+        {
+          folder: "amredi",
+        }
+      );
+
+      // Create the post with the uploaded image URL
+      const post = await Post.create({
+        title,
+        description,
+        image: {
+          publicId: result.public_id,
+          url: result.secure_url,
+        },
+        createdBy: user._id,
+      });
+
+      res.status(201).json({
+        message: "Post created successfully",
+        post,
+      });
+    } else {
+      const user = await userModel.findById(req.payload.aud);
+      // console.log(user);
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      // Access other form fields from req.body
+      const { title, description } = req.body;
+      // Create the post with the uploaded image URL
+      const post = await Post.create({
+        title,
+        description,
+        image: {
+          publicId: null,
+          url: null,
+        },
+        createdBy: user._id,
+      });
+
+      res.status(201).json({
+        message: "Post created successfully",
+        post,
+      });
     }
     // Query the database to retrieve user information
-    const user = await userModel.findById(req.payload.aud);
-    // console.log(user);
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-    // Access other form fields from req.body
-    const { title, description } = req.body;
-
-    // Convert image buffer to Base64 string
-    const imageBuffer = imageFile.buffer.toString("base64");
-
-    // Upload the image to Cloudinary as Base64 string
-    const result = await cloudinary.uploader.upload(
-      `data:${imageFile.mimetype};base64,${imageBuffer}`,
-      {
-        folder: "amredi",
-      }
-    );
-
-    // Create the post with the uploaded image URL
-    const post = await Post.create({
-      title,
-      description,
-      image: {
-        publicId: result.public_id,
-        url: result.secure_url,
-      },
-      createdBy: user._id,
-    });
-
-    res.status(201).json({
-      message: "Post created successfully",
-      post,
-    });
   } catch (error) {
     console.error("Error creating post:", error);
     res.status(500).json({ error: "Error creating post" });
